e002aa05650fc0faa8f78bdc013a9f199c5a73dc
baosloan <baosloan@gmail.com>
2024-01-31 10:27:11 +0800

    增加字符串交集函数和翻转函数

--- README.md
+++ README.md
@@ -25,3 +25,26 @@ func main() {
 	fmt.Println(t2) //2024-10-01 08:00:00 +0800 CST
 }
 ```
+
+## stringutil
+字符串工具包
+```go
+package main
+
+import (
+	"fmt"
+	"github.com/baosloan/utils/stringutil"
+)
+
+func main() {
+	//求交集
+	var s1 = []string{"jack", "rose", "jane"}
+	var s2 = []string{"michael", "jack", "rose"}
+	fmt.Println(stringutil.Intersect(s1, s2)) //[jack rose]
+	//字符串切片翻转(不改变原有切片)
+	var s3 = []string{"111", "222", "333", "444", "555", "666"}
+	var s4 = stringutil.Reverse(s3)
+	fmt.Println(s3) //[111 222 333 444 555 666]
+	fmt.Println(s4) //[666 555 444 333 222 111]
+}
+```
---
+++ stringutil/stringutil.go
@@ -0,0 +1,26 @@
+package stringutil
+
+// Intersect 求两个字符串切片的交集
+func Intersect(slice1, slice2 []string) []string {
+	m := make(map[string]struct{})
+	for _, v := range slice1 {
+		m[v] = struct{}{}
+	}
+	var result []string
+	for _, v := range slice2 {
+		if _, ok := m[v]; ok {
+			result = append(result, v)
+		}
+	}
+	return result
+}
+
+// Reverse 将字符串切片翻转(不改变原有切片)
+func Reverse(slice []string) []string {
+	var length = len(slice)
+	var result = make([]string, length)
+	for i := 0; i < length/2; i++ {
+		result[i], result[length-i-1] = slice[length-i-1], slice[i]
+	}
+	return result
+}
